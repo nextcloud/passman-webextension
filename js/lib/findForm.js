f031c410e5deabc28b627b805630debff692e96d
Nils <iudex-@users.noreply.github.com>
2018-02-04 04:35:39 +0100

    urgh

--- js/lib/findForm.js
+++ js/lib/findForm.js
@@ -1,9 +1,8 @@
 function fillPassword(user, password) {
     if(user){
-        document.querySelectorAll('input[type=text]').forEach(function(x){x.value=user});
-        document.querySelectorAll('input[type=email]').forEach(function(x){x.value=user});
+        document.querySelectorAll('input[type=text], input[type=email]').forEach(function(x){x.value=user;});
     }
     if(password) {
-        document.querySelectorAll('input[type=password]').forEach(function(x){ x.value=password});
+        document.querySelectorAll('input[type=password]').forEach(function(x){ x.value=password;});
     }
 }
