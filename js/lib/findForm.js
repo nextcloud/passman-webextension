db874c9b6932b0c30baa585e50c3a000b7c33366
Nils <iudex-@users.noreply.github.com>
2018-02-04 04:29:57 +0100

    lets also fill email inputs with the user
    accounts.google.com (and probably many more) uses input type email, so lets also fill that with the username/email address

--- js/lib/findForm.js
+++ js/lib/findForm.js
@@ -1,6 +1,7 @@
 function fillPassword(user, password) {
     if(user){
         document.querySelectorAll('input[type=text]').forEach(function(x){x.value=user});
+        document.querySelectorAll('input[type=email]').forEach(function(x){x.value=user});
     }
     if(password) {
         document.querySelectorAll('input[type=password]').forEach(function(x){ x.value=password});
