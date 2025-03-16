935d7497e613030721f179bc5cee7c20693e205c
binsky <timo@binsky.org>
2025-03-16 16:46:08 +0100

    try to improve content scripts logistic a bit

--- src/contents/index.ts
+++ src/contents/index.ts
@@ -1,19 +1,39 @@
 import { RemoteCallableFunctionNames, RemoteCallableFunctions } from "~contents/remoteCallableFunctions";
+import PasswordPicker from "./nested/PasswordPicker.svelte";
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log("[content script] Received message from background script:", message);
-
-    if (message.name === RemoteCallableFunctions.remoteFunctionCallMessageName) {
-        if (message.body.method && message.body.method in RemoteCallableFunctionNames) {
-            console.log("do remoteFunctionCall:", message.body.method);
-
-            const methodName = message.body.method as RemoteCallableFunctionNames;
-            const response = RemoteCallableFunctions.getRemoteCallableFunction(methodName)(message.body.args);
-            if (response) {
-                sendResponse(response);
-            }
-        }
-    }
+// password picker will be checked and initialized by PasswordPicker.svelte if it is in contents directory;
+// since we moved it to nested directory, we need to check for it here
+const app = new PasswordPicker({
+    target: document.body,
+    props: {}
 });
 
-// password picker will be checked and initialized by PasswordPicker.svelte
+function init() {
+    // Modified message listener with error handling
+    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+        try {
+            console.log("[content script] Received message from background script:", message);
+
+            if (message.name === RemoteCallableFunctions.remoteFunctionCallMessageName) {
+                if (message.body.method && message.body.method in RemoteCallableFunctionNames) {
+                    console.log("do remoteFunctionCall:", message.body.method);
+
+                    const methodName = message.body.method as RemoteCallableFunctionNames;
+                    const response = RemoteCallableFunctions.getRemoteCallableFunction(methodName)(message.body.args);
+                    if (response) {
+                        sendResponse(response);
+                    }
+                }
+            }
+            return true; // Keep the message channel open for async responses
+        } catch (e) {
+            console.error("[content script] Error processing message:", e);
+            return false;
+        }
+    });
+}
+
+// run init when document is ready
+document.addEventListener('DOMContentLoaded', () => {
+    init();
+});
