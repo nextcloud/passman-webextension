dd277bd18629772bc4c4492d73b21f3bc841f976
binsky <timo@binsky.org>
2025-06-07 00:35:18 +0200

    add melt-ui svelte ui library

--- svelte.config.mjs
+++ svelte.config.mjs
@@ -1,9 +1,14 @@
 import preprocess from 'svelte-preprocess';
+import { preprocessMeltUI, sequence } from '@melt-ui/pp'
 
 const config = {
-    preprocess: preprocess({
-        typescript: true
-    })
+    preprocess: sequence([
+        // ... other preprocessors
+        preprocess({
+            typescript: true
+        }),
+        preprocessMeltUI() // add to the end!
+    ])
 };
 
 export default config;
