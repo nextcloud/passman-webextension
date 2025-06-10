340f4fd0376430ee337d8e156455f833f49b6f4e
binsky <timo@binsky.org>
2025-06-10 23:09:59 +0200

    add standalone badge style in TagsView component

--- src/spa_partials/TagsView.svelte
+++ src/spa_partials/TagsView.svelte
@@ -13,3 +13,41 @@
         {/each}
     </div>
 {/if}
+
+<style>
+    .badge {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        transition-property: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-timing-function: cubic-bezier(0, 0, 0.2, 1);
+        transition-duration: 200ms;
+        height: 1.25rem /* 20px */;
+        font-size: 0.875rem /* 14px */;
+        line-height: 1.25rem /* 20px */;
+        width: fit-content;
+        padding-left: 0.563rem /* 9.008px */;
+        padding-right: 0.563rem /* 9.008px */;
+        border-width: 1px;
+        --tw-border-opacity: 1;
+        border-color: hsl(var(--b2) / var(--tw-border-opacity));
+        --tw-bg-opacity: 1;
+        background-color: hsl(var(--b1) / var(--tw-bg-opacity));
+        --tw-text-opacity: 1;
+        color: hsl(var(--bc) / var(--tw-text-opacity));
+        border-radius: var(--rounded-badge, 1.9rem /* 30.4px */);
+
+        margin: 0.125rem;
+    }
+    :where(.menu li) .badge {
+        justify-self: end;
+    }
+
+    .badge-outline {
+        border-color: currentColor;
+        --tw-border-opacity: 0.5;
+        background-color: transparent;
+        color: currentColor;
+    }
+</style>
