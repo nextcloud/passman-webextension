ad49c668682537ed6cc2b68277938fa5e737d6f5
binsky <timo@binsky.org>
2025-11-01 00:46:01 +0100

    fix missing cursor-pointer class for some buttons

--- src/spa_partials/CredentialEditSections/EditGeneral.svelte
+++ src/spa_partials/CredentialEditSections/EditGeneral.svelte
@@ -121,7 +121,7 @@
         {/if}
     </div>
     <div class="mt-2">
-        <button class="bg-slate-100 px-2 py-1 rounded-md hover:bg-slate-200" on:click={() => {
+        <button class="bg-slate-100 px-2 py-1 rounded-md hover:bg-slate-200 cursor-pointer" on:click={() => {
             showExtendedPasswordSettings = !showExtendedPasswordSettings;
         }}>
             {i18n.getMessage('toggle_extended_password_settings')}
@@ -155,7 +155,7 @@
                         {@const tagItem = tagsInput.getTagItem({ tag })}
                         <div {...tagItem.tag} class="flex items-center gap-1 px-2 py-1 bg-gray-100 rounded-md">
                             <span>{tag.value}</span>
-                            <button {...tagItem.deleteTrigger} aria-label={`remove ${tag.value} tag`} class="text-gray-500 hover:text-gray-700">
+                            <button {...tagItem.deleteTrigger} aria-label={`remove ${tag.value} tag`} class="text-gray-500 hover:text-gray-700 cursor-pointer">
                                 ×
                             </button>
                         </div>
--- src/spa_partials/InteractionElements/OnClickButton.svelte
+++ src/spa_partials/InteractionElements/OnClickButton.svelte
@@ -22,7 +22,7 @@
         {disabled}
         {tabindex}
         class="text-primary-light-button-text border border-gray-300 hover:bg-base-200 font-medium rounded-lg
-	text-sm dark:hover:bg-neutral {small
+	text-sm dark:hover:bg-neutral cursor-pointer {small
 		? 'px-2 py-1'
 		: 'px-3 py-2'} text-center w-auto disabled:opacity-70 disabled:pointer-events-none {additionalClasses}"
 >
